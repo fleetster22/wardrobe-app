b4f6bf079c61b7fb465c33158e2aff689b4b8bf0
Anaka Norfleet <fleetster22@gmail.com>
2023-07-19 13:46:05 -0400

    shoes models, views and urls completed

---
+++ shoes/api/shoes_rest/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from .views import list_bins, list_shoes, show_shoe
+
+urlpatterns = [
+    path("bins/", list_bins, name="list_bins"),
+    path("shoes/", list_shoes, name="list_shoes"),
+    path("shoes/<int:pk>/", show_shoe, name="list_shoes"),
+]
